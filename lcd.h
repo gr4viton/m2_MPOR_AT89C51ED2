24c72ee18e176ed9acb5f01948095bcfc8d1965f
gr4viton <lordmutty@gmail.com>
2014-04-24 10:52:06 +0200

    lcd removed

--- lcd.h
+++ lcd.h
@@ -2,16 +2,4 @@
 #define _LCD_H_
 
 
-#include "defines.h"
-#include "waitin.h"
-#include "dev_LCD_HD44780.h"
-
-
-
-void gpio_enter(uint8_t port, uint8_t bits, uint8_t val);
-void gpio_clear(uint8_t port, uint8_t bits);
-void gpio_set(uint8_t port, uint8_t bits);
-void gpio_port_write(uint8_t port, uint8_t portVal);
-
-void LCD_initPredef(void);
 #endif
